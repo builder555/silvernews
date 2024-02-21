e07e90d0b3bb121d51aafd04812e13383145d129
builder555 <85308587+builder555@users.noreply.github.com>
2024-02-20 21:51:49 -0500

    formatted

--- service-stories/api/db_interactor.py
+++ service-stories/api/db_interactor.py
@@ -1,13 +1,14 @@
 import json
 import os
 
+
 class DB:
     def __init__(self, path: str):
         self._file = path
         if not os.path.exists(self._file):
-            with open(self._file, 'w') as f:
+            with open(self._file, "w") as f:
                 json.dump([], f)
 
     def get_stories(self):
-        with open(self._file, 'r') as f:
+        with open(self._file, "r") as f:
             return json.load(f)
