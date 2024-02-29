e39b22492fcf6f9b186e319fe1df7de47b7181aa
builder555 <85308587+builder555@users.noreply.github.com>
2024-02-28 19:05:41 -0500

    formatted

--- service-stories/api/db_interactor.py
+++ service-stories/api/db_interactor.py
@@ -13,29 +13,32 @@ class DB:
         """Executes a given SQL query with optional parameters."""
         self.cursor.execute(query, params)
         self.connection.commit()
-    
+
     def _fetch_query(self, query, params=()):
         """Executes a given SQL query and returns fetched results."""
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
-    
+
     def _create_table(self, create_table_sql):
         """Creates a table with the given SQL statement."""
         self._execute_query(create_table_sql)
-    
+
     def _insert_data(self, insert_sql, data):
         """Inserts data into a table using the given SQL statement."""
         self._execute_query(insert_sql, data)
-    
+
     def _query_data(self, query, params=()):
         """Queries data from the database."""
         return self._fetch_query(query, params)
-    
+
     def __del__(self):
         self.connection.close()
 
     def get_stories(self):
-        return self._fetch_query('SELECT * FROM `stories`')
+        return self._fetch_query("SELECT * FROM `stories`")
 
     def add_story(self, story: dict):
-        self._execute_query('INSERT INTO `stories` (url, title, content, poster) VALUES (?, ?, ?, ?)', (story["url"], story["title"], story["content"], story["poster"]))
+        self._execute_query(
+            "INSERT INTO `stories` (url, title, content, poster) VALUES (?, ?, ?, ?)",
+            (story["url"], story["title"], story["content"], story["poster"]),
+        )
