609330b00c1186acf2e98849df9bedea3343e83a
builder555 <85308587+builder555@users.noreply.github.com>
2024-03-29 19:24:33 -0400

    reset css

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -7,3 +7,33 @@ import TheHeader from './components/TheHeader.vue'
   <TheHeader />
   <RouterView />
 </template>
+<style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-weight: normal;
+}
+html, body {
+  height: 100%;
+}
+body.dark {
+  --bg-color: #000;
+  --bg-secondary-color: #131316;
+  --font-color: #f5f5f5;
+  --color-grey: #ccc;
+  --color-darkGrey: #777;
+}
+:root {
+  --color-warning: #ffcc00;
+}
+/* .button.warning {
+  background-color: var(--color-warning);
+  color: var(--font-color);
+} */
+.warning {
+  color: var(--color-warning);
+}
+</style>
