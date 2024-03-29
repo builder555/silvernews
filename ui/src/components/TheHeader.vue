470c69430dab7c5cdb70d4684c137637449066ec
builder555 <85308587+builder555@users.noreply.github.com>
2024-03-29 19:26:41 -0400

    header with menu

--- ui/src/components/TheHeader.vue
+++ ui/src/components/TheHeader.vue
@@ -1,9 +1,31 @@
 <script setup>
+import { computed } from 'vue';
+import { useStore } from '@/stores/theme';
+const store = useStore();
+const darkIcon = 'https://icongr.am/feather/sun.svg?size=128&color=ffcc00';
+const lightIcon = 'https://icongr.am/feather/moon.svg?size=128&color=000';
+
+const themeIcon = computed(() => {
+  return store.isDark ? darkIcon : lightIcon;
+});
 </script>
 
 <template>
   <header>
-    <h1>Header</h1>
+    <nav class="nav">
+      <div class="nav-left">
+        <a class="brand">SilverNews</a>
+        <div class="tabs">
+          <a>New</a>
+          <a class="active">Submit</a>
+        </div>
+      </div>
+      <div class="nav-right">
+        <a class="button clear icon-only" @click="store.toggleTheme">
+          <img :src="themeIcon" />
+        </a>
+        <a class="active">Log In</a>
+      </div>
+    </nav>
   </header>
 </template>
-
