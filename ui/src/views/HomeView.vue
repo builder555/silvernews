b223a2958cc249262910aca08891366ccf174ed3
builder555 <85308587+builder555@users.noreply.github.com>
2024-04-04 18:24:08 -0400

    proper fields

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup>
 const stories = [
-  {id:1, title: 'test story 1'},
-  {id:2, title: 'test story 2'},
-  {id:3, title: 'test story 3'},
+  {id: 1, title: 'test story 1', poster: 'user1', url: 'https://www.google.com'},
+  {id: 2, title: 'test story 2', poster: 'user2', content: 'hello there'},
+  {id: 3, title: 'test story 3', poster: 'user3', url: 'https://www.yahoo.com'},
 ];
 </script>
 
