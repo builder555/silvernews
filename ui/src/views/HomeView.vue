38415367b9ecc5dcf41beda1c0ec6d26bbfa3016
builder555 <85308587+builder555@users.noreply.github.com>
2024-04-17 21:02:45 -0400

    basic styling

--- ui/src/views/HomeView.vue
+++ ui/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup>
 const stories = [
-  {id: 1, title: 'test story 1', poster: 'user1', url: 'https://www.google.com'},
-  {id: 2, title: 'test story 2', poster: 'user2', content: 'hello there'},
-  {id: 3, title: 'test story 3', poster: 'user3', url: 'https://www.yahoo.com'},
+  {id: 1, votes: 10, title: 'test story 1', poster: 'user1', url: 'https://www.google.com'},
+  {id: 2, votes: 20, title: 'test story 2', poster: 'user2', content: 'hello there'},
+  {id: 3, votes: 30, title: 'test story 3', poster: 'user3', url: 'https://www.yahoo.com'},
 ];
 </script>
 
@@ -11,6 +11,18 @@ const stories = [
     <div 
       v-for="story in stories" 
       :key="story.id"
+      class="story"
     >{{ story.title }}</div>
   </main>
 </template>
+<style scoped>
+  main {
+    padding: 0.5em;
+  }
+  .story {
+    background-color: var(--bg-secondary-color);
+    border-radius: 5px;
+    margin: 0.3rem;
+    padding: 0.3rem;
+  }
+</style>
